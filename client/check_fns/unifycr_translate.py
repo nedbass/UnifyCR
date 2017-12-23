LLNL/UnifyFS

commit 713f9fd8664d9cf4ad6886374923094ab5b5d867

add gotcha and test programs to build, support make dist (#36)
- Add m4 script to find and link with gotcha.  Build and install
  libunifycr_gotcha library.
- Build test_write and test_read example programs using both --wrap
  and gotcha intercept methods.
- Update autotools files to support make distcheck to build release
  tarballs.
- Add foreign to automake init call in configure.ac.
- Move custom m4 files from client/maint/config to m4 new directory.
- Update README.md to document new build process and --wrap/gotcha
  intercept examples.
- Drop old buildme scripts.
- Drop LLNL-specific scripts to run test cases, e.g., runserver.sh.
- Drop wrappers using stat64 which failed to compile on travis:
  __xstat64, __lxstat64, __fxstat64.  Someone can bring these back
  in the future if needed.
- Fix typo (void* --> void) in return type for setbuf function.

client/src/gotcha_map_unifycr_list.h
@@ -12,9 +12,7 @@ UNIFYCR_DEF(unlink, int, (const char *path));
 UNIFYCR_DEF(remove, int, (const char *path));
 UNIFYCR_DEF(stat, int, (const char *path, struct stat *buf));
 UNIFYCR_DEF(__xstat, int, (int vers, const char *path, struct stat *buf));
-UNIFYCR_DEF(__xstat64, int, (int vers, const char *path, struct stat64 *buf));
 UNIFYCR_DEF(__lxstat, int, (int vers, const char *path, struct stat *buf));
-UNIFYCR_DEF(__lxstat64, int, (int vers, const char *path, struct stat64 *buf));
 UNIFYCR_DEF(creat, int, (const char* path, mode_t mode));
 UNIFYCR_DEF(creat64, int, (const char* path, mode_t mode));
 UNIFYCR_DEF(open, int, (const char *path, int flags, ...));
@@ -38,12 +36,11 @@ UNIFYCR_DEF(mmap, void*, (void *addr, size_t length, int prot, int flags, int fd
 UNIFYCR_DEF(msync, int, (void *addr, size_t length, int flags));
 UNIFYCR_DEF(mmap64, void*, (void *addr, size_t length, int prot, int flags, int fd, off_t offset));
 UNIFYCR_DEF(__fxstat, int, (int vers, int fd, struct stat *buf));
-UNIFYCR_DEF(__fxstat64, int, (int vers, int fd, struct stat64 *buf));
 UNIFYCR_DEF(close, int, (int fd));
 UNIFYCR_DEF(fopen, FILE*, (const char *path, const char *mode));
 UNIFYCR_DEF(freopen, FILE*, (const char *path, const char *mode, FILE *stream));
 UNIFYCR_DEF(setvbuf, int, (FILE* stream, char* buf, int type, size_t size));
-UNIFYCR_DEF(setbuf, void*, (FILE* stream, char* buf));
+UNIFYCR_DEF(setbuf, void, (FILE* stream, char* buf));
 UNIFYCR_DEF(ungetc, int, (int c, FILE *stream));
 UNIFYCR_DEF(fgetc, int, (FILE *stream));
 UNIFYCR_DEF(fputc, int, (int c, FILE *stream));
@@ -95,9 +92,7 @@ struct gotcha_binding_t wrap_unifycr_list[] = {
 	{ "remove", UNIFYCR_WRAP(remove), &UNIFYCR_REAL(remove) },
 	{ "stat", UNIFYCR_WRAP(stat), &UNIFYCR_REAL(stat) },
 	{ "__xstat", UNIFYCR_WRAP(__xstat), &UNIFYCR_REAL(__xstat) },
-	{ "__xstat64", UNIFYCR_WRAP(__xstat64), &UNIFYCR_REAL(__xstat64) },
 	{ "__lxstat", UNIFYCR_WRAP(__lxstat), &UNIFYCR_REAL(__lxstat) },
-	{ "__lxstat64", UNIFYCR_WRAP(__lxstat64), &UNIFYCR_REAL(__lxstat64) },
 	{ "creat", UNIFYCR_WRAP(creat), &UNIFYCR_REAL(creat) },
 	{ "creat64", UNIFYCR_WRAP(creat64), &UNIFYCR_REAL(creat64) },
 	{ "open", UNIFYCR_WRAP(open), &UNIFYCR_REAL(open) },
@@ -121,7 +116,6 @@ struct gotcha_binding_t wrap_unifycr_list[] = {
 	{ "msync", UNIFYCR_WRAP(msync), &UNIFYCR_REAL(msync) },
 	{ "mmap64", UNIFYCR_WRAP(mmap64), &UNIFYCR_REAL(mmap64) },
 	{ "__fxstat", UNIFYCR_WRAP(__fxstat), &UNIFYCR_REAL(__fxstat) },
-	{ "__fxstat64", UNIFYCR_WRAP(__fxstat64), &UNIFYCR_REAL(__fxstat64) },
 	{ "close", UNIFYCR_WRAP(close), &UNIFYCR_REAL(close) },
 	{ "fopen", UNIFYCR_WRAP(fopen), &UNIFYCR_REAL(fopen) },
 	{ "freopen", UNIFYCR_WRAP(freopen), &UNIFYCR_REAL(freopen) },

configure.ac
@@ -3,12 +3,14 @@ dnl This file is a part of UnifyCR. Please see LICENSE for the license
 dnl information.
 dnl Process this file with autoconf to produce a configure script.
 
-AC_INIT([unifycr], [1.0.0], [unifycr@llnl.gov])
+AC_INIT([unifycr], [0.1.0], [unifycr@llnl.gov])
 AC_CONFIG_SRCDIR([configure.ac])
 AC_CONFIG_HEADERS([config.h])
-AC_CONFIG_MACRO_DIR([./client/maint/config/])
+AC_CONFIG_MACRO_DIR([m4])
 
-AM_INIT_AUTOMAKE([tar-pax subdir-objects])
+AM_INIT_AUTOMAKE([foreign tar-pax subdir-objects])
+
+AM_MAINTAINER_MODE([disable])
 
 AC_PROG_CC([mpicc])
 AC_PROG_AWK
@@ -146,6 +148,9 @@ PKG_CHECK_MODULES([LEVELDB], [leveldb], ,
         ],
         AC_MSG_ERROR(['leveldb is required to build unifycr.'])))
 
+# look for gotcha library, sets GOTCHA_INCLUDE, GOTCHA_LIB
+UNIFYCR_AC_GOTCHA
+
 # libc functions wrapped by unifycr
 
 CP_WRAPPERS+="-Wl,-wrap,access"
@@ -281,6 +286,7 @@ AC_CONFIG_FILES([Makefile
                  server/src/Makefile
                  client/Makefile
                  client/src/Makefile
+                 client/tests/Makefile
                  client/unifycr.pc])
 
 UNIFYCR_VERSION=${PACKAGE_VERSION}

autogen.sh
@@ -77,7 +77,7 @@ fi
 
 ## Do the autogeneration
 echo Running ${ACLOCAL}...
-$ACLOCAL -I ./contrib/aclocal
+$ACLOCAL
 echo Running ${AUTOHEADER}...
 $AUTOHEADER
 echo Running ${TOOL}...

client/Makefile.am
@@ -1,4 +1,4 @@
-SUBDIRS = src
+SUBDIRS = src tests
 
 bin_SCRIPTS = unifycr-config
 

client/buildme_bgq
@@ -1,14 +1,0 @@
-#!/bin/bash
-
-set -x
-
-./prepare
-./configure \
-  --prefix=`pwd`/install_bgq \
-  --disable-ld-preload \
-  CC=mpixlc CFLAGS="-g -O0 -DMACHINE_BGQ" || exit 1
-#  CC=mpixlc CFLAGS="-O3 -DMACHINE_BGQ" || exit 1
-
-make clean || exit 1
-make || exit 1
-make install || exit 1

client/buildme_opt
@@ -1,38 +1,0 @@
-#!/bin/bash
-
-. /usr/local/tools/dotkit/init.sh
-use -q ic-11.1.046 
-
-set -x
-
-# if nothing is passed in then the interception method will be --wrap,
-# but run like: ./buildme_opt -DUNIFYCR_GOTCHA or ./buildme_opt -DUNIFYCR_PRELOAD
-# for the gotcha or preload interception methods  
-intercept=$1
-
-# path link to gotcha -- only needs to be set if you ran with
-# ./buildme_opt -DUNIFYCR_GOTCHA         
-gotcha_path="/g/g0/sikich1/gotcha_inst"
-if [ ! -z ${intercept} ]; then
-
-        echo "using ${intercept} for I/O interception"
-
-        if [ ${intercept} == "-DUNIFYCR_GOTCHA" ]; then
-                link_gotcha="-I${gotcha_path}/include -L${gotcha_path}/lib -lgotcha"
-        else 
-                link_gotcha=""
-        fi
-else
-        echo "using --wrap for I/O interception"
-fi
-
-./prepare
-./configure \
-  --enable-ld-preload \
-  --prefix=`pwd`/install \
-  CC=mpicc CFLAGS="-g -O0 -fPIC ${intercept} -DENABLE_NUMA_POLICY ${link_gotcha}" || exit 1
-
-make clean || exit 1
-make || exit 1
-make install || exit 1
-chmod +x unifycr-config

client/buildme_zlib
@@ -1,16 +1,0 @@
-#!/bin/bash
-
-set -x
-
-if [ ! -f zlib-1.2.7.tar.gz ] ; then
-  wget zlib.net/zlib-1.2.7.tar.gz
-fi
-
-rm -rf zlib-1.2.7
-tar -zxf zlib-1.2.7.tar.gz
-cd zlib-1.2.7
-
-export CC=mpixlc
-./configure --prefix=`pwd`/install
-make CC=mpixlc
-make install

client/src/Makefile.am
@@ -1,13 +1,48 @@
-lib_LTLIBRARIES = libunifycr.la
+lib_LTLIBRARIES = libunifycr.la libunifycr_gotcha.la
+
 libunifycrdir = $(includedir)
+libunifycr_gotchadir = $(includedir)
 
-libunifycr_la_SOURCES = unifycr-fixed.c unifycr-stack.c \
-                        unifycr-stdio.c unifycr-sysio.c
-
-libunifycr_la_CPPFLAGS = -I$(top_srcdir)/client
-
-libunifycr_la_LDFLAGS = -version-info $(LIBUNIFYCR_LT_VERSION)
-
-# TODO: this public header file is broken. It requires unifycr-internal.h.
 libunifycr_HEADERS = unifycr.h
+libunifycr_la_SOURCES = \
+  unifycr-fixed.c \
+  unifycr-fixed.h \
+  unifycr-stack.c \
+  unifycr-stack.h \
+  unifycr-stdio.c \
+  unifycr-stdio.h \
+  unifycr-sysio.c \
+  unifycr-sysio.h \
+  unifycr.c \
+  unifycr.h \
+  unifycr-defs.h \
+  unifycr-internal.h \
+  unifycr-wrap.h \
+  uthash.h \
+  utlist.h
+
+libunifycr_la_CPPFLAGS = -I$(top_builddir)/client
+libunifycr_la_LDFLAGS = -version-info $(LIBUNIFYCR_LT_VERSION) -lcrypto -lrt -lpthread
+
+libunifycr_gotcha_HEADERS = unifycr.h
+libunifycr_gotcha_la_SOURCES = \
+  unifycr-fixed.c \
+  unifycr-fixed.h \
+  unifycr-stack.c \
+  unifycr-stack.h \
+  unifycr-stdio.c \
+  unifycr-stdio.h \
+  unifycr-sysio.c \
+  unifycr-sysio.h \
+  unifycr.c \
+  unifycr.h \
+  gotcha_map_unifycr_list.h \
+  unifycr-defs.h \
+  unifycr-internal.h \
+  unifycr-wrap.h \
+  uthash.h \
+  utlist.h
+
+libunifycr_gotcha_la_CPPFLAGS = -DUNIFYCR_GOTCHA -I$(top_builddir)/client $(GOTCHA_CFLAGS)
+libunifycr_gotcha_la_LDFLAGS = -version-info $(LIBUNIFYCR_LT_VERSION) $(GOTCHA_LDFLAGS) -lgotcha -lcrypto -lrt -lpthread
 

nedbass/UnifyCR

commit 3bd6d62267016fa52e11f9d954357d0b2900ec98

Fix access beyond array bounds in unifycrfs_mount
Fix the NFUNCS macro (and rename it to GOTCHA_NFUNCS) in
gotcha_map_unifycr_list.h so it resolves to the correct number of
functions in wrap_unifycr_list. The previous hard-coded value of 79
exceeded the actual list size of 76, causing a potential undefined
behavior bug and the compiler warning below. This bug was introduced
by commit 713f9fd8664d ("add gotcha and test programs to build,
support make dist (#36)")' which removed the three wrapped functions
__xstat64, __lxstat64, __fxstat6 from wrap_unifycr_list but didn't
update NFUNCS.

unifycr.c: In function unifycrfs_mount
unifycr.c:1694:48: warning: iteration 76u invokes undefined behavior
[-Waggressive-loop-optimizations]
             if (*(void
**)(wrap_unifycr_list[i].function_address_pointer) == 0) {
                                                ^
unifycr.c:1693:9: note: containing loop
         for (i = 0; i < NFUNCS; i++) {
         ^

Update the unifycr_translate.py script that generates
gotcha_map_unifycr_list.h so it computes GOTCHA_NFUNCS rather than
hard-coding it. That way is someone manually updates the function
list in the future it the computed size will still be correct.

Remove references to __xstat64, __lxstat64, and __fxstat6 from
client/check_fns/unifycr_list.txt and configure.ac since they're
no longer wrapped.

Add ARRAY_SIZE exemption to style checker so it doesn't complain
about use of sizeof(array) / sizeof(array[0]) to compute array size.

client/check_fns/unifycr_translate.py
@@ -47,9 +47,6 @@ def write_gotcha_file(filename, func_list, modulename):
         for function in func_list:
                 f.write(function.macro_def + "(" + function.name + ", " + function.ret_type + 
                         "," " (" + function.params[0] + "));" + "\n") 
-        
-        # get number of functions we are wrapping for gotcha struct
-	f.write("\n#define NFUNCS " + str(len(func_list)) + "\n\n")
 
 	# write map struct
 	f.write("struct gotcha_binding_t wrap_" + modulename + "[] = {\n")
@@ -57,7 +54,10 @@ def write_gotcha_file(filename, func_list, modulename):
 		f.write("\t{ " + function.name_quote + ", " + function.wrapper + ", &" + function.wrapee + " },\n")
 	f.write("};\n")
 
-        # close struct and file
+	# define number of functions we are wrapping for gotcha struct
+	f.write("\n#define GOTCHA_NFUNCS (sizeof(wrap_unifycr_list) / sizeof(wrap_unifycr_list[0]))\n")
+
+	# close struct and file
 	f.close()
 
 def main(modulename):

client/src/gotcha_map_unifycr_list.h
@@ -88,8 +88,6 @@ UNIFYCR_DEF(getwc, wint_t, (FILE *stream));
 UNIFYCR_DEF(putwc, wint_t, (wchar_t c, FILE *stream));
 UNIFYCR_DEF(ungetwc, wint_t, (wint_t c, FILE *stream));
 
-#define NFUNCS 79
-
 struct gotcha_binding_t wrap_unifycr_list[] = {
     { "access", UNIFYCR_WRAP(access), &UNIFYCR_REAL(access) },
     { "mkdir", UNIFYCR_WRAP(mkdir), &UNIFYCR_REAL(mkdir) },
@@ -168,3 +166,5 @@ struct gotcha_binding_t wrap_unifycr_list[] = {
     { "putwc", UNIFYCR_WRAP(putwc), &UNIFYCR_REAL(putwc) },
     { "ungetwc", UNIFYCR_WRAP(ungetwc), &UNIFYCR_REAL(ungetwc) },
 };
+
+#define GOTCHA_NFUNCS (sizeof(wrap_unifycr_list) / sizeof(wrap_unifycr_list[0]))

client/src/unifycr.c
@@ -1684,13 +1684,13 @@ static int unifycr_init(int rank)
 #ifdef UNIFYCR_GOTCHA
         enum gotcha_error_t result;
 
-        result = gotcha_wrap(wrap_unifycr_list, NFUNCS, "unifycr");
+        result = gotcha_wrap(wrap_unifycr_list, GOTCHA_NFUNCS, "unifycr");
         if (result != GOTCHA_SUCCESS) {
             debug("gotcha_wrap returned %d\n", (int) result);
         }
 
         int i;
-        for (i = 0; i < NFUNCS; i++) {
+        for (i = 0; i < GOTCHA_NFUNCS; i++) {
             if (*(void **)(wrap_unifycr_list[i].function_address_pointer) == 0) {
                 printf("This function name failed to be wrapped: %s\n",
                        wrap_unifycr_list[i].name);

configure.ac
@@ -164,9 +164,7 @@ CP_WRAPPERS+=",-wrap,rename"
 CP_WRAPPERS+=",-wrap,truncate"
 CP_WRAPPERS+=",-wrap,stat"
 CP_WRAPPERS+=",-wrap,__lxstat"
-CP_WRAPPERS+=",-wrap,__lxstat64"
 CP_WRAPPERS+=",-wrap,__xstat"
-CP_WRAPPERS+=",-wrap,__xstat64"
 
 CP_WRAPPERS+=",-wrap,creat"
 CP_WRAPPERS+=",-wrap,creat64"
@@ -192,7 +190,6 @@ CP_WRAPPERS+=",-wrap,mmap64"
 CP_WRAPPERS+=",-wrap,munmap"
 CP_WRAPPERS+=",-wrap,msync"
 CP_WRAPPERS+=",-wrap,__fxstat"
-CP_WRAPPERS+=",-wrap,__fxstat64"
 CP_WRAPPERS+=",-wrap,close"
 
 # FILE* functions

scripts/checkpatch.sh
@@ -21,6 +21,7 @@ checkpatch_ignore+=",MISSING_SIGN_OFF"  # Signed-off-by: line is optional
 checkpatch_ignore+=",FILE_PATH_CHANGES" # Don't nag about updating MAINTAINERS
 checkpatch_ignore+=",CONST_STRUCT"      # Don't nag about const structs
 checkpatch_ignore+=",SPLIT_STRING"      # Allow long strings to be split
+checkpatch_ignore+=",ARRAY_SIZE"        # Don't require use of ARRAY_SIZE macro
 
 checkpatch_cmd+=" --ignore $checkpatch_ignore"
 